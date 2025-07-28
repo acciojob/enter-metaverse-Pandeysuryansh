a7bdcc4c46f206a6d934887aa5cb35e6b37e074e
Pandeysuryansh <167824825+Pandeysuryansh@users.noreply.github.com>
2025-07-28 22:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const statuspara = document.getElementById('status');
+const btn = document.getElementById('eneterBtn');
+
+enterButton.addEventListener('click',function(){
+      statuspara.outerHTML = '<h1 id="status">Entered Metaverse</h1>';
+});
